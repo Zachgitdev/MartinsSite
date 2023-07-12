c0b7e308cd278b03801cc8ebaadd60e94bb3d7af
Zach <zachdevelop99@gmail.com>
2023-07-12 03:03:57 +0100

    added some horizontal lines for flair

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,14 @@ const App = () => {
           <Hero />
         </div>
         <About />
+        <div className='h-1 bg-beige w-[80%] mx-auto'></div>
         <PolishedPlaster />
+        <div className='h-1 bg-beige w-[80%] mx-auto'></div>
         <DecorativeWalls />
         <div id='Plastering'></div>
         <Hero2 />
         <TilePage />
+        <div className='h-1 bg-beige w-[80%] mx-auto'></div>
         <Gallery />
         <div className='relative z-0'>
           <Contact />
