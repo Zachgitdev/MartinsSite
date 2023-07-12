828d585dc2fee878f2c3928f98c2367d55bb9e45
TastyShoe <12201664+Benorli@users.noreply.github.com>
2023-07-12 18:32:16 +0100

    :art: object cover and overflow, css to fix aspect ratio

--- src/components/TilePage.jsx
+++ src/components/TilePage.jsx
@@ -6,17 +6,17 @@ import { SectionWrapper } from '../hoc'
 const TilePage = () => {
   return (
     <div>
-        {/* <h2 className={styles.sectionHeadText}>Plastering</h2> */}
-        <ul className='flex justify-center grid grid-cols-3 gap-5'>
-            {plastering.map((plaster) => (
-                <li>
-                    <img src={plaster.image} className='h-[200px] min-w-full' />
-                    <p className={`${styles.sectionSubText}`}>{plaster.title}</p>
-                    <div className={`${styles.sectionHeadLine} mb-5`}></div>
-                    <p>{plaster.text}</p>
-                </li>
-            ))}
-        </ul>
+      {/* <h2 className={styles.sectionHeadText}>Plastering</h2> */}
+      <ul className='flex justify-center grid grid-cols-3 gap-5'>
+        {plastering.map((plaster) => (
+          <li>
+            <img src={plaster.image} className='h-[200px] min-w-full overflow-hidden object-cover' />
+            <p className={`${styles.sectionSubText}`}>{plaster.title}</p>
+            <div className={`${styles.sectionHeadLine} mb-5`}></div>
+            <p>{plaster.text}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
